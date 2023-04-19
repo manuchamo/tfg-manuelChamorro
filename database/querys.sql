07a17a0c93cce5d591ea374b4eab3ed914cb7789
manuchamo <mchamorrom97@gmail.com>
2023-04-19 21:59:06 +0200

    database

--- database/querys.sql
+++ database/querys.sql
@@ -6,4 +6,5 @@ create database dogsorcats;
 CREATE USER tfgadmin WITH PASSWORD 'tfgdatabase';
 GRANT ALL PRIVILEGES ON DATABASE dogsorcats to tfgadmin;
 
-psql -U tfgadmin -d dogsorcats -h 10.0.20.210 
+-- Conectividad consola psql
+psql -U tfgadmin -d dogsorcats -h 10.0.20.20 
